bfe16f45a7aa3a6885b5bc7e05a9ae3d0599ae49
Derek <derek.s.leung.407@gmail.com>
2019-01-20 18:25:37 -0800

    Finished create actions on other user-type controllers

--- app/controllers/api/v1/managers_controller.rb
+++ app/controllers/api/v1/managers_controller.rb
@@ -1,13 +1,28 @@
 class Api::V1::ManagersController < ApplicationController
 
   def create
+    manager = Manager.new manager_params
+    user = User.new user_params
+    user.userable_type = "Manager"
 
+    if manager.save
+      if user.save
+        manager.user = user
+        render json: manager
+      end
+    else
+      render json: {status: 422, errors: [manager.errors.full_messages, user.errors.full_messages]}
+    end
   end
 
   private
 
   def manager_params
-
+    params.require(:manager).permit()
   end
-  
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name, :phone_number)
+  end
+
 end
--- app/controllers/api/v1/walkers_controller.rb
+++ app/controllers/api/v1/walkers_controller.rb
@@ -1,13 +1,27 @@
 class Api::V1::WalkersController < ApplicationController
 
   def create 
+    walker = Walker.new walker_params
+    user = User.new user_params
+    user.userable_type = "Walker"
 
+    if walker.save
+      if user.save
+        walker.user = user
+        render json: walker
+      end
+    else
+      render json: {status: 422, errors: [walker.errors.full_messages, user.errors.full_messages]}
+    end
   end
 
   private
 
-  def walkers_params
-
+  def walker_params
+    params.require(:walker).permit(:status)
   end
 
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name, :phone_number)
+  end
 end
