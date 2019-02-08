cebd19e5e8ae6d2f811ef23b1fd93978f03c63d3
chrischu5 <chris.chu.yc@gmail.com>
2019-02-08 15:48:56 -0800

    Create User model file

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  validates :first_name, presence: true,
+  validates :email, presence: true,
+                    uniqueness: true,
+                    format: VALID_EMAIL_REGEX
+  
+  def full_name
+    "#{first_name} #{last_name}".strip
+  end
+end
---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :user do
+    email { "MyString" }
+    password { "MyString" }
+    first_name { "MyString" }
+    last_name { "MyString" }
+    role { "MyString" }
+    phone_number { "MyString" }
+  end
+end
